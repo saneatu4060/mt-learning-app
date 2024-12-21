438ab3b0f7bc1e84f307b1ac947689c5f6bd5924
saneatu4060 <unnko4060@gmail.com>
2024-12-21 23:51:43 +0900

    deploy error fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,14 +7,14 @@ import { getAuthSession } from "@/lib/nextauth";
 import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {};
+
 
 export const metadata = {
     title: "ダッシュボード | MT-Learning",
     description: "過去問を解いて学習を始めましょう！",
 };
 
-const Dasboard = async (props: Props) => {
+const Dasboard = async () => {
     const session = await getAuthSession();
     if (!session?.user) {
         redirect("/");
@@ -24,16 +24,15 @@ const Dasboard = async (props: Props) => {
         <main className="p-8 mx-auto max-w-7xl">
             <div className="flex items-center">
                 <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
-                {/* <DetailsDialog /> */}
+
             </div>
 
             <div className="grid gap-4 mt-4 md:grid-cols-2">
-                {/* <QuizMeCard />
-                <HistoryCard /> */}
+
             </div>
             <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
-                {/* <HotTopicsCard />
-                <RecentActivityCard /> */}
+
+
             </div>
         </main>
     );
