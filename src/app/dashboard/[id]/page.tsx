fbfaa7e43a0c5faa276c28c9eed80e0ef57e9276
saneatu4060 <unnko4060@gmail.com>
2025-01-14 23:00:14 +0900

    fix error newfavicon

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,35 +1,12 @@
-// import ModeSelector from '@/components/ModeSelect'
-// import { getAuthSession } from '@/lib/nextauth';
-// import { redirect } from 'next/navigation';
-
-// export default async function TestModePage({ params }: { params: { id: string } }) {
-//     const session = await getAuthSession();
-//     if (!session?.user) {
-//         redirect("/");
-//     }
-//     return <ModeSelector testNumber={parseInt(params.id)} />
-// }
-
-import { useRouter } from 'next/router';
-import ModeSelector from '@/components/ModeSelect';
+import ModeSelector from '@/components/ModeSelect'
 import { getAuthSession } from '@/lib/nextauth';
 import { redirect } from 'next/navigation';
 
-export default async function TestModePage() {
-    const router = useRouter();
-    const { id } = router.query; // 動的ルートパラメータを取得
-
-    // 認証セッションの確認
+export default async function TestModePage({ ...params }) {
     const session = await getAuthSession();
     if (!session?.user) {
         redirect("/");
-        return null;
     }
-
-    // パラメータが取得されるまでレンダリングを待つ
-    if (!id || Array.isArray(id)) {
-        return <p>Loading...</p>;
-    }
-
-    return <ModeSelector testNumber={parseInt(id)} />;
+    return <ModeSelector testNumber={parseInt(params.id)} />
 }
+
