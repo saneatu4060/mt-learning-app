7c4ebc5320a056bb7e4609df4e2f7625e93f6e29
saneatu4060 <unnko4060@gmail.com>
2025-01-14 22:14:03 +0900

    fix build error

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,14 +1,9 @@
 import Link from 'next/link'
 import { Card } from '@/components/ui/card'
-import { prisma } from "@/lib/db"
+
 
 export default async function Category() {
-    const categories = await prisma.question.findMany({
-        distinct: ['category'],  // 重複するカテゴリは1つだけ取得
-        select: {
-            category: true  // カテゴリのみ取得
-        }
-    })
+
     return (
 
 
@@ -16,8 +11,8 @@ export default async function Category() {
             <h1 className="text-2xl font-bold mb-4 text-gray-800">演習テストリスト</h1>
             <p className="mb-8 text-gray-600">受けたい演習テストを選択してください。</p>
 
-            {categories.map((num) => (
-                <Link href={`/dashboard/${Category.category}`} key={index}>
+            {[1, 2, 3].map((num) => (
+                <Link href={`/dashboard/${num}`} key={num}>
                     <Card className="p-4 mb-4 hover:bg-gray-50 transition-colors cursor-pointer bg-white/80 backdrop-blur-sm">
                         <h2 className="text-xl text-gray-600">演習テスト{num}</h2>
                     </Card>
